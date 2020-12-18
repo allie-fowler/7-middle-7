dcfbb4adb73c83b3da310c32a1c48183f3955d51
Allie <67930016+allie-fowler@users.noreply.github.com>
2020-12-18 10:58:21 -0500

    Update get_price_history.py

--- scripts/get_price_history.py
+++ scripts/get_price_history.py
@@ -4,7 +4,7 @@ import getopt, sys
 
 def usage():
     print("Usage:")
-    print("get_price_history.py [ -h | --help ] [ -v | --verbose ] <SYMBOL>")
+    print("get_price_history.py [ -h | --help ] [ -v | --verbose ] -s <SYMBOL>")
 
 def lookup(my_symbol):
     from googlefinance.get import get_datum
@@ -13,7 +13,9 @@ def lookup(my_symbol):
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+        opts, args = getopt.getopt(sys.argv[1:], "ho:v:s", ["help", "output="])
+        print ('Opts are ' + opts)
+        print ('Args are ' + args)
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)  # will print something like "option -a not recognized"
@@ -21,23 +23,26 @@ def main():
         sys.exit(2)
     symbol = ""
     verbose = False
-    for o, a in opts:
-        if o == "-v":
-            verbose = True
-        elif o in ("-h", "--help"):
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
             usage()
             sys.exit()
-        else:
-            symbol = a
+        if opt == "-v":
+            verbose = True
+        if opt == "-s":
+            symbol = arg
+        else
+            print ('Option was ' + opt)
+            print ('Argument was ' + arg)       
+            usage()
+            sys.exit
     
-    if verbose:
-        print('Symbol is ' + symbol)  
-        
-    if symbol == "":      
-      usage()
-      sys.exit()
+    if symbol == "":
+          usage()
+          sys.exit()
           
     lookup(symbol.upper()) 
 
 if __name__ == "__main__":
     main()
+
