a6228ae532c466f6876f054247acf061013c3ec8
Allie <67930016+allie-fowler@users.noreply.github.com>
2020-12-21 14:28:56 -0500

    Create define-and-clear-counters.sh

---
+++ scripts/define-and-clear-counters.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+# Set threshold that defines "sideways"
+# May have to change this for various symbols
+
+# 3% should be written as 0.03, for example
+sideways_threshold=0.03
+
+for month in ( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ) 
+do
+  for period in ( front middle back )
+  do
+    export ${month}_${period}=0
+  done
+done
+
+set | grep front
+set | grep middle
+set | grep back
