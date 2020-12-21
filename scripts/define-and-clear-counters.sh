54976d60a32493c67e43ae4535c997649e8755d3
Allie <67930016+allie-fowler@users.noreply.github.com>
2020-12-21 14:32:50 -0500

    Update define-and-clear-counters.sh

--- scripts/define-and-clear-counters.sh
+++ scripts/define-and-clear-counters.sh
@@ -6,9 +6,9 @@ set -x
 # 3% should be written as 0.03, for example
 sideways_threshold=0.03
 
-for month in ( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ) 
+for month in Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
 do
-  for period in ( front middle back )
+  for period in  front middle back
   do
     export ${month}_${period}=0
   done
