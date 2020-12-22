f93b331b218a6fc3f164f2edc969b37aa51c0bb0
Allie <67930016+allie-fowler@users.noreply.github.com>
2020-12-22 09:31:15 -0500

    Update cycle_and_compare.sh

--- scripts/cycle_and_compare.sh
+++ scripts/cycle_and_compare.sh
@@ -1,5 +1,77 @@
 #!/bin/bash
 
+is_past () {
+# parameters:  today's date YYYY-MM-DD, comparison year YYYY, comparison month MM, comparison day DD
+  if [ "$1" <= "${2}-${3}-${4}" ]
+  then 
+    if [ "$verbose" = true ]; then echo "This date is after today.  Not valid." ; fi
+    return 1;  # 1 is false
+  else 
+    if [ "$verbose" = true ]; then echo "This date is past.  Valid for evaluation." ; fi
+    return 0;  # 0 is true
+  fi
+}
+
+latest_trade_close_of_range() {
+# Parameters:  day1, day2, month, year
+local day1=$1
+local day2=$2
+local month=$3
+local year=$4
+
+  for ((i=$day2;i<=$day1;i--)); do
+    if [ "$verbose" = true ]; then echo "Evaluating ${month} ${i}, ${year}" ; fi
+    
+    if [ is_past("${today}" "${year}" "${month}" "${i}" ) ]
+    then 
+      # Discontinue this iteration of the for-day loop and go on with the next value
+      continue
+    else
+      # get the back_b close and break the loop.  if not there, discontinue this iteration and go on with next value
+      if $(grep "${year}-${month}-${i}" ${symbol}.csv) 
+      then
+        # Grab the adjusted close
+         
+        latest_close=$( grep "${year}-${month}-${i}" ${symbol}.csv | awk ' { print $6 } ' )
+        echo "${latest_close}"
+        break  # We found the day, no need to keep iterating
+      else
+        continue
+      fi
+    fi
+  done
+}  # latest_trade_close_of_range
+
+earliest_trade_close_of_range() {
+# Parameters:  day1, day2, month, year
+local day1=$1
+local day2=$2
+local month=$3
+local year=$4
+
+  for ((i=$day2;i>=$day1;i++)); do
+    if [ "$verbose" = true ]; then echo "Evaluating ${month} ${i}, ${year}" ; fi
+    
+    if [ is_past("${today}" "${year}" "${month}" "${i}" ) ]
+    then 
+      # Discontinue this iteration of the for-day loop and go on with the next value
+      continue
+    else
+      # get the front close and break the loop.  if not there, discontinue this iteration and go on with next value
+      if $(grep "${year}-${month}-${i}" ${symbol}.csv) 
+      then
+        # Grab the adjusted close
+         
+        earliest_close=$( grep "${year}-${month}-${i}" ${symbol}.csv | awk ' { print $6 } ' )
+        echo "${earliest_close}"
+        break  # We found the day, no need to keep iterating
+      else
+        continue
+      fi
+    fi
+  done
+}  # earliest_trade_close_of_range
+
 this_year=$(echo ${today} | awk ' { print $1 } ')
 # Cycle through the last 11 years
 for (( year=${this_year}; year>=${year}-11; year-- ))
@@ -7,29 +79,19 @@ do
   for month in 1 2 3 4 5 6 7 8 9 10 11 12
   do
     echo "Processing for Month: ${month} Year: ${year}"
-    # Get back.  
-      for day in 31 30 29 28 27
-      do
-        if [ "$today" <= "${year}-${month}-${day}" ]
-        then 
-          echo "This date is after today.  Not valid."
-          continue
-        else
-          # get the back_b close and break the loop.  if error continue
-          grep "${year}-${month}-${day}" ${symbol}.csv || continue
-          # Grab the adjusted close
-          back_b_close=$( grep "${year}-${month}-${day}" ${symbol}.csv | awk ' { print $6 } ' ) && break
-        fi
+    
+    # Process for last 7 days of month
+    # get close of latest trading day of range  
+    latest_close=$(latest_trade_close_of_range(27 31 "${month}" "${year}"))
+      
+    # get close of earliest trading day of range 
+    earliest_close=$(earliest_trade_close_of_range(20 25 "${month}" "${year}"))
+    
+    # Increment appropriate counter
         
-        # Find first market day after 20th. continue if later than today otherwise get the close
-        for day in 20 21 22 23 24 25
-        do
-          # Find last market day of month. continue if later than today otherwise get the close
-          # compare the two closes and increment the appropriate counter
-        done
-        
-    # Get middle
+    # Process for middle 7
       # Find first market day on or after 10th
-    # Get front
-  done
-done
+      
+    # Process for front 7
+  done  # month
+done  # year
