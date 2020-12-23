9f475d5f62a7b9b1219133ae1b46f66b67665b71
Allie <67930016+allie-fowler@users.noreply.github.com>
2020-12-23 09:02:44 -0500

    Update cycle_and_compare.sh

--- scripts/cycle_and_compare.sh
+++ scripts/cycle_and_compare.sh
@@ -4,7 +4,7 @@
 # Tallies the counters
 
 function usage {
-        echo "./$(basename $0) [-v] [-h] "
+        echo "./$(basename "$0") [-v] [-h] "
         echo "   Steps through past 11 years of history and compares each 7-m-7 segment of each past month."
 }
 
@@ -27,16 +27,16 @@ local day2=$2
 local month=$3
 local year=$4
 
-  for ((i=$day2;i<=$day1;i--)); do
-    if [ "$verbose" = true ]; then echo "Evaluating ${month} ${i}, ${year}" ; fi
+  for (( i=$day2; i<=$day1; i-- )); do
+    if [ "$verbose" = true ]; then echo "Evaluating $month $i, $year" ; fi
     
-    if is_past "${today}" "${year}" "${month}" "${i}" 
+    if is_past "${today}" "$year" "$month" "$i" 
     then 
       # Discontinue this iteration of the for-day loop and go on with the next value
       continue
     else
       # get the back_b close and break the loop.  if not there, discontinue this iteration and go on with next value
-      if grep "${year}-${month}-${i}" "${symbol}".csv
+      if grep "$year-$month-$i" "${symbol}".csv
       then
         # Grab the adjusted close
          
@@ -57,20 +57,20 @@ local day2=$2
 local month=$3
 local year=$4
 
-  for ((i=$day2;i>=$day1;i++)); do
-    if [ "$verbose" = true ]; then echo "Evaluating ${month} ${i}, ${year}" ; fi
+  for (( i=$day2; i>=$day1; i++ )); do
+    if [ "$verbose" = true ]; then echo "Evaluating $month $i, $year" ; fi
     
-    if is_past "${today}" "${year}" "${month}" "${i}" 
+    if is_past "${today}" "$year" "$month" "$i" 
     then 
       # Discontinue this iteration of the for-day loop and go on with the next value
       continue
     else
       # get the front close and break the loop.  if not there, discontinue this iteration and go on with next value
-      if grep "${year}-${month}-${i}" ${symbol}.csv
+      if grep "$year-$month-$i" "${symbol}".csv
       then
         # Grab the adjusted close
          
-        earliest_close=$( grep "${year}-${month}-${i}" ${symbol}.csv | awk ' { print $6 } ' )
+        earliest_close=$( grep "$year-$month-$i" "${symbol}".csv | awk ' { print $6 } ' )
         echo "${earliest_close}"
         break  # We found the day, no need to keep iterating
       else
@@ -84,7 +84,7 @@ local year=$4
 verbose=1
 # list of arguments expected in the input
 # We use "$@" instead of $* to preserve argument-boundary information
-ARGS=$(getopt -o 'h:v' --long 'help::,verbose' -- "$@") || exit
+ARGS=$(getopt -o 'h:v:s' --long 'help::,verbose::,symbol' -- "$@") || exit
 eval "set -- $ARGS"
 
 while true; do
@@ -93,17 +93,20 @@ while true; do
             ((verbose=0)); shift;;
       (-h|--help)
             usage;;
+      (-s|--symbol)
+        symbol="${OPTARG}"
+        ;;
       (--)  shift; break;;
-      (*)   exit 1;;           # error
+      (*)   echo "Invalid argument" ; exit 1;;           # error
     esac
 done
 remaining=("$@")
-if [ "$verbose" = true ]; then echo "Arguments not processed yet: ${remaining}" ; fi
+if [ "$verbose" = true ]; then echo "Arguments not processed yet: ${remaining[@]}" ; fi
 
 source scripts/define-and-clear-counters.sh
-this_year=$(echo ${today} | awk ' { print $1 } ')
+this_year=$(echo "${today}" | awk ' { print $1 } ')
 # Cycle through the last 11 years
-for (( year=${this_year}; year>=${year}-11; year-- ))
+for (( year=$this_year; year>=$year-11; year-- ))
 do
   for month in 1 2 3 4 5 6 7 8 9 10 11 12
   do
