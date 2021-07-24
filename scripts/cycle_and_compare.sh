070353f27d06c7b8cb0079e94cd785afe3206c6e
Allie <67930016+allie-fowler@users.noreply.github.com>
2021-07-24 17:13:40 -0400

    Update cycle_and_compare.sh

--- scripts/cycle_and_compare.sh
+++ scripts/cycle_and_compare.sh
@@ -12,7 +12,7 @@ function usage {
 is_past () {
 # parameters:  today's date YYYY-MM-DD, comparison year YYYY, comparison month MM, comparison day DD
 # (( $(date -d "2014-12-01T21:34:03+02:00" +%s) < $(date -d "2014-12-01T21:35:03+02:00" +%s) ))
-  if [[ ! (( $(date -d "$1" +%s) > $(date -d "${3} ${4} ${2}" +%s) )) ]]
+  if [[ (( $(date +%s) < $(date -d "${2} ${3} ${1}" +%s) )) ]]
   then 
     if [ "$verbose" = 0 ]; then echo "This date is after today.  Not valid." ; fi
     return 1;  # 1 is false
@@ -108,7 +108,7 @@ do
   do
     echo "Processing for Month: $month Year: $year"
     
-    result=$(is_past "${today}" "$year" "$month" "1") 
+    result=$(is_past "$year" "$month" "1") 
     if [ "$result" == "0" ]
     then 
       # Throw away the date if it's in the future and go on with the next value
